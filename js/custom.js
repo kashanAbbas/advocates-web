d4b9620db5c6ad291f879b261a76b4320b79d955
kashanAbbas <94021758+kashanAbbas@users.noreply.github.com>
2021-11-26 05:44:47 +0500

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,105 @@
+$(function () {
+$('.homslider').slick({
+  infinite: true,
+  arrows: true,
+  dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1
+});
+
+});
+
+
+$(function () {
+$('.blogslid').slick({
+  arrows: true,
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+    });
+
+    $(function () {
+$('.testmonilz').slick({
+  dots: true,
+  arrows: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+    });
+
+
+	$(function () {
+		  new WOW().init();
+	});
+
+
+
+
+// slicknav-script
+
+$(function(){
+$('#menu').slicknav();
+}); 
